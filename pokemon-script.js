499b310e458b239c952e90254af565e8e1e09e77
Patrick Gogolin <patrick.gogolin@hotmail.com>
2024-06-01 01:28:53 +0200

    first commit

--- pokemon-script.js
+++ pokemon-script.js
@@ -1,14 +1,13 @@
 BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 let loadedPokemon = [];
-let amountPokemons = 21;
+let amountPokemons = 550;
 
 async function init() {
     showSpinner();
     try {
         await loadPokemon();
         hideSpinner();
-        renderPokemonCard();
         renderLoadMoreButton();
     } catch (error) {
         console.error('Error initializing data:', error);
@@ -18,27 +17,26 @@ async function init() {
 
 async function loadPokemon() {
     try {
-        for (let i = 1; i < amountPokemons; i++) {
+        for (let i = 520; i < amountPokemons; i++) {
             let url = BASE_URL + i;
             let response = await fetch(url);
             let pokemons = await response.json();
-            loadedPokemon.push(pokemons)
+            loadedPokemon.push(pokemons);
         }
-        console.log(loadedPokemon);
-        renderPokemonCard()
+        renderPokemonCard();
     }
     catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
-function renderPokemonCard() {  // Hier weitermachen 
+function renderPokemonCard() { 
     let content = document.getElementById('content');
     content.innerHTML = '';
 
     for (let i = 0; i < loadedPokemon.length; i++) {
         const pokemon = loadedPokemon[i];
-        const name = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1); // kovertiert den ersten Buchstaben zu Groß
+        const name = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
         const height = pokemon['height'] / 10;
         const weight = pokemon['weight'] / 10;
         content.innerHTML += generatePokemonCardHtml(i, name, pokemon, height, weight);
@@ -49,21 +47,24 @@ function renderPokemonCard() {  // Hier weitermachen
 }
 
 function renderLoadMoreButton() {
-    document.getElementById('load-more-button').innerHTML += /*html*/`
-        <button onclick="loadMorePokemon()"><img src="img/pokeball.png" alt="Pokeball"></button>;`
+    document.getElementById('load-more-button').innerHTML = generateLoadMoreButtonHtml();
 }
 
 async function loadMorePokemon() {
-    showSpinner();
+    showLoadMoreSpinner();
     try {
         loadedPokemon.length = 0;
         amountPokemons += 20;
         await loadPokemon();
-        hideSpinner();
+        renderLoadMoreButton();
     } catch (error) {
         console.error('Error initializing data:', error);
         hideSpinner();
     }
+}
+
+function showLoadMoreSpinner() { 
+    document.getElementById('load-more-button').innerHTML = generateLoadMoreSpinnerHtml();
 }
 
 function getPokemonAbilities(i, pokemon){
@@ -93,23 +94,164 @@ function hideSpinner() {
 }
 
 function openDialog(i) {
+    renderSingleCard(i);
     let overlayer = document.getElementById('overlayer');
     let preventScroll = document.getElementById('no-scroll');
     overlayer.classList.remove('d-none');
     preventScroll.classList.add('no-scroll');
+}
 
+function renderSingleCard(i) {
+    const pokemon = loadedPokemon[i];
+    const firstType = pokemon['types'][0]['type']['name'];
+    const name = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
+    contentSingleCard = document.getElementById('single-card-container');
+    contentMobileSwitchButtons = document.getElementById('mobile-switch-image-container');
+
+    renderSingleCardContentBasedOnIndex(i, pokemon, firstType, name);
+    renderChangeImageButtonsForMobile(i);
+    
+    getTypesForSingleCard(i, pokemon);
+    getStatsForSingleCard(i, pokemon);
+}
+
+function renderSingleCardContentBasedOnIndex(i, pokemon, firstType, name) {
+    if (i === 0) {
+        contentSingleCard.innerHTML = renderSingleCardHtmlFirstImage(i, pokemon, firstType, name);
+    } else if (i === loadedPokemon.length - 1) {
+        contentSingleCard.innerHTML = renderSingleCardHtmlLastImage(i, pokemon, firstType, name);
+    } else {
+        contentSingleCard.innerHTML = renderSingleCardHtmlEnabledButton(i, pokemon, firstType, name);
+    }
+}
+
+function renderChangeImageButtonsForMobile(i) {
+    if (i === 0) {
+        contentMobileSwitchButtons.innerHTML = renderMobileSwitchButtonHtmlFirstImage(i);
+    } else if (i === loadedPokemon.length - 1) {
+        contentMobileSwitchButtons.innerHTML = renderMobileSwitchButtonHtmlLastImage(i);
+    } else {
+        contentMobileSwitchButtons.innerHTML = renderMobileSwitchButtonHtmlEnabledButton(i);
+    }
+}
+
+function getTypesForSingleCard(i, array) {
+    for (let x = 0; x <  array['types'].length; x++) {
+        const type = array['types'][x]['type']['name'];
+        const typeContainer = document.getElementById(`types${i}`);
+        typeContainer.innerHTML += generateSingleCardType(type);
+    }
+}
+
+function getStatsForSingleCard(i, array) {
+    for (let x = 0; x < array['stats'].length; x++) {
+        const statContainer = document.getElementById(`stats-${i}`);
+        const stat = array['stats'][x]['base_stat'];
+        const statName = array['stats'][x]['stat']['name'];
+        statContainer.innerHTML += generateStatsHtml(statName, stat);
+    }
+}
+
+function showMoves(i){
+    let statMoveHeadline = document.getElementById('stat-moves-headline');
+    let statContainer = document.getElementById(`stats-${i}`);
+    statContainer.innerHTML = '';
+    statMoveHeadline.innerHTML = 'Moves:';
+    for (let x = 0; x < 4; x++) {
+        const move = loadedPokemon[i]['moves'][x]['move']['name'];
+        statContainer.innerHTML += generateMovesHtml(move);
+    }
+}
+
+function showStats(i){
+    let statMoveHeadline = document.getElementById('stat-moves-headline');
+    let statContainer = document.getElementById(`stats-${i}`);
+    statContainer.innerHTML = '';
+    statMoveHeadline.innerHTML = 'Stats:';
+      for (let x = 0; x < loadedPokemon[i]['stats'].length; x++) {
+        const stat = loadedPokemon[i]['stats'][x]['base_stat'];
+        const statName = loadedPokemon[i]['stats'][x]['stat']['name'];
+        statContainer.innerHTML += generateStatsHtml(statName, stat);
+    }
 }
 
 function closeDialog() {
+    let enableScroll = document.getElementById('no-scroll');
     let overlayer = document.getElementById('overlayer');
     overlayer.classList.add('d-none');
-
+    enableScroll.classList.remove('no-scroll');
 }
 
 function playSound(soundPath) {
-        const audio = new Audio(soundPath);
-        audio.play();
+    const audio = new Audio(soundPath);
+    audio.play();
 }
+
+function nextImage(i) {
+    if(i === loadedPokemon.length -2){
+        let image = (i + 1);
+        renderSingleCard(image);
+    }
+    else {
+        let image = (i + 1);
+        renderSingleCard(image);
+    }
+}
+
+function previousImage(i) {
+    if (i === 1) {
+        let image = (i - 1);
+        renderSingleCard(image);
+    } else {
+        let image = (i - 1);
+        renderSingleCard(image);
+    }
+}
+
+function filterNames() {
+    let search = document.getElementById('search-input-field').value.toLowerCase().trim();
+    let content = document.getElementById('content'); 
+    content.innerHTML = '';
+
+    conditionsForResultOfSearchInput(search, content);
+}
+
+function conditionsForResultOfSearchInput(search, content) {
+    if (search.length >= 3) { 
+        searchPokemon(search, content);
+
+    } else if (search === '') {
+        renderPokemonCard();
+        renderLoadMoreButton();
+    }
+}
+
+function searchPokemon(search, content) {
+    let found = false;
+    for (let i = 0; i < loadedPokemon.length; i++) {
+        let pokemonName = loadedPokemon[i]['name'];
+
+        if (pokemonName.includes(search)) {
+            found = true;
+            renderFilteredPokemon(i, content)
+        }
+    }
+    if (!found) {
+        content.innerHTML = generateContentNotFoundHtml();
+    }
+}
+
+function renderFilteredPokemon(i, content) {
+    const pokemon = loadedPokemon[i];
+    const name = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
+    const height = pokemon['height'] / 10;
+    const weight = pokemon['weight'] / 10;
+    content.innerHTML += generatePokemonCardHtml(i, name, pokemon, height, weight);
+    getPokemonAbilities(i, pokemon);
+    getPokemonTypes(i, pokemon);
+}
+
+
 
 
 
--- template.js
+++ template.js
@@ -16,10 +16,23 @@ function generatePokemonCardHtml(i, name, pokemon, height, weight) {
             <div class="height-and-weight-container">
                 <p>Height: ${height} m</p>
                 <p>Weight: ${weight} kg</p>
-                <button class="sound-button" id="play-sound-button${i}" onclick="playSound('${pokemon['cries']['legacy']}')"><img src="img/klang.png" alt="PokemonLogo"></button>
+                <button class="sound-button" id="play-sound-button${i}" onclick="playSound('${pokemon['cries']['latest']}')"><img src="img/klang.png" alt="PokemonLogo"></button>
 
             </div>
         </div>`;
+}
+
+function generateLoadMoreButtonHtml() {
+    return /*html*/` 
+        <button onclick="loadMorePokemon()"><img src="img/pokeball.png" alt="Pokeball">
+        </button>`;
+}
+
+function generateLoadMoreSpinnerHtml() {
+    return /*html*/` 
+    <div id="loading-spinner" class="spinner-render-more-pokemon">
+            <img src="img/pokeball.png" alt="Pokeball" class="spinner-image">
+        </div>;`
 }
 
 function generateAbilityHtml(ability) {
@@ -34,4 +47,125 @@ function generateTypeHtml(typeClassName, type) {
     <div class="${typeClassName} type-padding">
      ${type}
     </div>`;
+}
+
+function renderSingleCardHtmlFirstImage(i, pokemon, firstType, name) {
+    return /*html*/`
+     <button id="previous-button" class= "previous-image-button-disabled" onclick="previousImage(${i})"disabled><span class="button-arrow"><b><</b></span></button>
+    <div class="single-card type-${firstType}" id="card-${i}">
+        <div class="name-and-id-container">
+            <h1>${name}</h1>
+            <p class="font-size"> <b>#${pokemon['id']}</b></p>
+        </div>
+        <div class="pokemon-image-container">
+            <img class="pokemon-image" src="${pokemon['sprites']['other']['showdown']['front_default']}" alt="${pokemon['name']}">
+            <div class="types" id="types${i}">
+            </div>
+        </div>
+        <div class="stats-and-move-button-container">
+            <button onclick="showStats(${i})" class="bg-${firstType}">Stats</button>
+            <button onclick="showMoves(${i})" class="bg-${firstType}">Moves</button>
+        </div>
+        <div class="stats">
+                <h3 id="stat-moves-headline">Stats:</h3>
+                <div class="moves" id="stats-${i}">
+                </div>
+        </div>
+    </div>
+    <button id="next-button" class="next-image-button"  onclick="nextImage(${i})"><span class="button-arrow"><b>></b></span></button>`;
+}
+
+function renderSingleCardHtmlLastImage(i, pokemon, firstType, name) {
+    return /*html*/`
+     <button id="previous-button" class= "previous-image-button" onclick="previousImage(${i})"><span class="button-arrow"><b><</b></span></button>
+    <div class="single-card type-${firstType}" id="card-${i}">
+        <div class="name-and-id-container">
+            <h1>${name}</h1>
+            <p class="font-size"> <b>#${pokemon['id']}</b></p>
+        </div>
+        <div class="pokemon-image-container">
+            <img class="pokemon-image" src="${pokemon['sprites']['other']['showdown']['front_default']}" alt="${pokemon['name']}">
+            <div class="types" id="types${i}">
+            </div>
+        </div>
+        <div class="stats-and-move-button-container">
+            <button onclick="showStats(${i})" class="bg-${firstType}">Stats</button>
+            <button onclick="showMoves(${i})" class="bg-${firstType}">Moves</button>
+        </div>
+        <div class="stats">
+                <h3 id="stat-moves-headline">Stats:</h3>
+                <div class="moves" id="stats-${i}">
+                </div>
+        </div>
+    </div>
+    <button id="next-button" class="next-image-button-disabled"  onclick="nextImage(${i})"disabled><span class="button-arrow"><b>></b></span></button>`;
+}
+
+function renderSingleCardHtmlEnabledButton(i, pokemon, firstType, name) {
+    return /*html*/`
+     <button id="previous-button" class= "previous-image-button" onclick="previousImage(${i})"><span class="button-arrow"><b><</b></span></button>
+    <div class="single-card type-${firstType}" id="card-${i}">
+        <div class="name-and-id-container">
+            <h1>${name}</h1>
+            <p class="font-size"> <b>#${pokemon['id']}</b></p>
+        </div>
+        <div class="pokemon-image-container">
+            <img class="pokemon-image" src="${pokemon['sprites']['other']['showdown']['front_default']}" alt="${pokemon['name']}">
+            <div class="types" id="types${i}">
+            </div>
+        </div>
+        <div class="stats-and-move-button-container">
+            <button onclick="showStats(${i})" class="bg-${firstType}">Stats</button>
+            <button onclick="showMoves(${i})" class="bg-${firstType}">Moves</button>
+        </div>
+        <div class="stats">
+                <h3 id="stat-moves-headline">Stats:</h3>
+                <div class="moves" id="stats-${i}">
+                </div>
+        </div>
+    </div>
+    <button id="next-button" class="next-image-button"  onclick="nextImage(${i})"><span class="button-arrow"><b>></b></span></button>`;
+    
+}
+
+function renderMobileSwitchButtonHtmlFirstImage(i) {
+    return /*html*/`
+    <button id="previous-button-mobile" class= "previous-image-button-disabled" onclick="previousImage(${i})"disabled><span class="button-arrow"><b><</b></span></button>
+    <button id="next-button-mobile" class="next-image-button"  onclick="nextImage(${i})"><span class="button-arrow"><b>></b></span></button>`;
+
+}
+
+function renderMobileSwitchButtonHtmlLastImage(i) {
+    return /*html*/`
+    <button id="previous-button-mobile" class= "previous-image-button" onclick="previousImage(${i})"><span class="button-arrow"><b><</b></span></button>
+    <button id="next-button-mobile" class="next-image-button-disabled"  onclick="nextImage(${i})" disabled><span class="button-arrow"><b>></b></span></button>`;
+}
+
+function renderMobileSwitchButtonHtmlEnabledButton(i) {
+    return /*html*/`
+    <button id="previous-button-mobile" class= "previous-image-button" onclick="previousImage(${i})"><span class="button-arrow"><b><</b></span></button>
+    <button id="next-button-mobile" class="next-image-button"  onclick="nextImage(${i})"><span class="button-arrow"><b>></b></span></button>`;
+}
+
+function generateSingleCardType(type) {
+    return /*html*/`
+        <p class="bg-${type} type-padding">${type}</p>`;
+}
+
+function generateStatsHtml(statName, stat) {
+    return /*html*/`
+    <label for="file"><b>${statName}:</b> <b>${stat}</b></label>
+    <progress id="file" value="${stat}" max="275"></progress>`;
+}
+
+function generateMovesHtml(move) {
+    return /*html*/` 
+        <p>${move}</p>`;
+}
+
+function generateContentNotFoundHtml() {
+    return /*html*/`
+     <div class="pokemon-not-found-container">
+                <p>Leider kein Pokémon gefunden. Lade mehr Pokemon und versuche deine Suche erneut oder übrprüfe deine Sucheingabe</p>
+            </div>`;
 }
